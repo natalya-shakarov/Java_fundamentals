6993f4d22260619e0d448a0f6ca032b7738b4cf5
natalya-shakarov <natalya_yadgarova@yahoo.com>
2021-01-13 12:55:07 -0500

    exercise 3 under data types operators in inteliJ

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,16 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int crab = 1+1;
+        System.out.println(crab);
+        int bar = 1-1;
+        System.out.println(bar);
+        int pin = 3*20;
+        System.out.println(pin);
+        int har = 30/10;
+        System.out.println(har);
+        int ttt = 10%6;
+        System.out.println(ttt);
 
     }
 
