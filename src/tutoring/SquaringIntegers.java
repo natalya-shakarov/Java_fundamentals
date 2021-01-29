b66190e314b9909f3d228c9d9daecb23331e9bdf
natalya-shakarov <natalya_yadgarova@yahoo.com>
2021-01-29 11:56:37 -0500

    squaring integers

---
+++ src/tutoring/SquaringIntegers.java
@@ -0,0 +1,9 @@
+package tutoring;
+
+public class SquaringIntegers {
+    public static void main(String[] args) {
+        for(int x = 1; x <= 5; x++) {
+            System.out.println(x + " squared = " + (x * x));
+        }
+    }
+}
