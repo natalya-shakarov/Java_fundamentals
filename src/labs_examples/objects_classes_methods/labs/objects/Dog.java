98f199cd3afd769e28e0a36d71c40ebba20e0956
natalya-shakarov <natalya_yadgarova@yahoo.com>
2021-02-11 21:41:45 -0500

    Exercise 3 of section of 12

---
+++ src/labs_examples/objects_classes_methods/labs/objects/Dog.java
@@ -0,0 +1,31 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+public class Dog {
+
+    private String name;
+    private int weight;
+    private String color;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Dog(String name, int weight, String color) {
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/labs_examples/objects_classes_methods/labs/objects/DogController.java
@@ -0,0 +1,9 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+public class DogController {
+    public static void main(String[] args) {
+        Dog firstDog = new Dog("Fido");
+        Dog secondDog = new Dog("Lucy", 20);
+        Dog thirdDog = new Dog("Spike", 50, "brown");
+    }
+}
